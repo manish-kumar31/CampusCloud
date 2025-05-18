8b17fe498ef5bbd38d93e9755da97dec37bdca2e
Ankit Kumar <ankitanand5675@gmail.com>
2025-05-18 13:53:10 +0530

    admin dashboard

--- frontend/frontend/src/pages/Admin/SettingsProfile.jsx
+++ frontend/frontend/src/pages/Admin/SettingsProfile.jsx
@@ -14,11 +14,9 @@ import {
 
 const SettingsProfile = () => {
   const teacherInfo = {
-    name: 'John Doe',
-    email: 'johndoe@example.com',
-    phone: '123-456-7890',
-    address: '123 Main St, City, Country',
-    qualification: 'Master of Education',
+    name: 'Graphic Era',
+    address: 'Clement Town, Dehradun ,Uttarakhand',
+
   };
 
   return (
@@ -30,17 +28,12 @@ const SettingsProfile = () => {
         <ProfileHeader>Profile Details</ProfileHeader>
         <ProfileDetails>
           <ProfileLabel>Name:</ProfileLabel>
-          <ProfileInfo>{teacherInfo.name}</ProfileInfo>
-          <ProfileLabel>Email:</ProfileLabel>
-          <ProfileInfo>{teacherInfo.email}</ProfileInfo>
-          <ProfileLabel>Phone:</ProfileLabel>
-          <ProfileInfo>{teacherInfo.phone}</ProfileInfo>
+          <ProfileInfo aria-brailleroledescription=''>{teacherInfo.name}</ProfileInfo>
+         
           <ProfileLabel>Address:</ProfileLabel>
           <ProfileInfo>{teacherInfo.address}</ProfileInfo>
-          <ProfileLabel>Qualification:</ProfileLabel>
-          <ProfileInfo>{teacherInfo.qualification}</ProfileInfo>
         </ProfileDetails>
-        <EditButton>Edit Profile</EditButton>
+        {/* <EditButton>Edit Profile</EditButton> */}
       </Content>
     </ProfileContainer>
   );
